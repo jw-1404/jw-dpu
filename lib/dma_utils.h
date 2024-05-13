2d353e1b27083003c21f93217cbaef4032ba4282
Yong Zhou <zyong06@gmail.com>
2024-05-13 16:12:37 +0800

    [add] lib directory

--- lib/dma_utils.h
+++ lib/dma_utils.h
@@ -1,19 +1,19 @@
-#pragma  once
+#pragma once
 
-#include <stdio.h>
-#include <stdint.h>
-#include <unistd.h>
 #include <errno.h>
-#include <time.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <sys/types.h>
-#include <unistd.h>
 #include <time.h>
+#include <unistd.h>
 
 uint64_t getopt_integer(char *optarg);
 
-ssize_t read_to_buffer(char *fname, int fd, char *buffer, uint64_t size, uint64_t base);
+ssize_t read_to_buffer(char *fname, int fd, char *buffer, uint64_t size,
+                       uint64_t base);
 
-ssize_t write_from_buffer(char *fname, int fd, char *buffer, uint64_t size, uint64_t base);
+ssize_t write_from_buffer(char *fname, int fd, char *buffer, uint64_t size,
+                          uint64_t base);
 
 ssize_t aio_read_to_buffer(char *devname, int fd, char *buffer, uint64_t size);
 
