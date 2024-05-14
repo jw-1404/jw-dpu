ddffd20fcaab5f8e6060cdb09f2b9421a5d1a6e8
Yong Zhou <zyong06@gmail.com>
2024-05-14 14:46:00 +0800

    [add] asio_dpu_test scripts

---
+++ scripts/asio_dpu_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+infile=$1
+outfile=$2
+transferSize=$3
+transferCount=$4
+
+testError=0
+
+rm -f ../$outfile
+../src/asio_from_device -o $outfile -s $transferSize -c $transferCount &
+
+../src/asio_to_device -i ../$infile -s $transferSize -c $transferCount &
+
+# Wait for the current transactions to complete
+echo "Info: Wait the for current transactions to complete."
+wait
+
+# Verify that the written data matches the read data.
+echo "Info: Checking data integrity."
+cmp $outfile $infile
+returnVal=$?
+if [ ! $returnVal == 0 ]; then
+    echo "Error: The data written did not match the data that was read."
+    testError=1
+else
+    echo "Info: Data check passed for c2h and h2c channel"
+fi
+
+# Exit with an error code if an error was found during testing
+if [ $testError -eq 1 ]; then
+  echo "Error: Test completed with Errors."
+  exit 1
+fi
+
+# Report all tests passed and exit
+echo "Info: All PCIe DMA streaming tests passed."
+exit 0
